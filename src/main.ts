9ff7f7567383182eb864885810b3d67d35d7c7df
Brodino <brodino96@gmail.com>
2025-03-18 04:57:35 +0100

    feat: implemented warn module

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { Bot } from "./modules/bot"
 import TempRole from "./modules/tempRole"
+import Warn from "./modules/warn"
 import Config from "./utils/config"
 
 class Main {
 	private bot: Bot = new Bot()
-	private tempRole: TempRole | undefined
 
 	public async init() {
 
 		if (Config.tempRole.enabled) {
 			this.bot.addModule(TempRole)
+		}
+
+		if (Config.moderation.enabled) {
+			this.bot.addModule(Warn)
 		}
 
 		await this.bot.login()

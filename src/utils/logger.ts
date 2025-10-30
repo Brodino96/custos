0f759d24e6ee239832a1c3a288cd6c98b2ac8aab
Brodino <brodino96@gmail.com>
2025-10-30 18:35:54 +0100

    fix(logger): spacing issues in the text

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,20 +4,20 @@ export default class Logger {
     constructor(module: string) {
         this.module = module
     }
+
+    public info(txt: string) {
+        console.log(`[INFO] ${this.module}: ${txt}`)
+    }
     
     public error(txt: string) {
-        console.log("\x1b[31m", `[ERROR] ${this.module}: ${txt}`)
+        console.log(`\x1b[31m[ERROR] ${this.module}: ${txt}`)
     }
 
     public success(txt: string) {
-        console.log("\x1b[32m", `[SUCCESS] ${this.module}:  ${txt}`)
-    }
-
-    public info(txt: string) {
-        console.log(`[INFO] ${this.module}:  ${txt}`)
+        console.log(`\x1b[32m[SUCCESS] ${this.module}: ${txt}`)
     }
 
     public warn(txt: string) {
-        console.log("\x1b[33m", `[WARNING] ${this.module}:  ${txt}`)
+        console.log(`\x1b[33m[WARNING] ${this.module}: ${txt}`)
     }
 }

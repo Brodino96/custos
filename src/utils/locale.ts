796961ce79368cb16fa76c9dfa199b75f6b7a65c
Vincent Acanfora <brodino96@gmail.com>
2025-11-25 14:53:29 +0100

    refactor(Locale): switched to readonly object

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -1,10 +1,10 @@
-enum Generic {
-    noPermission = "⛔ You don't have permission to do this",
-    noSource = "⛔ Somehow you don't exist",
-    noTarget = "⛔ Target user is null",
-    dbFailure = "⛔ Failed to interact with the database",
+const Locale = Object.freeze({
+    generic: {
+        noPermission: "⛔ You don't have permission to do this",
+        noSource: "⛔ Somehow you don't exist",
+        noTarget: "⛔ Target user is null",
+        dbFailure: "⛔ Failed to interact with the database",
+    }
 }
-
-export default class Locale {
-    public static readonly generic = Generic
-}
+)
+export default Locale

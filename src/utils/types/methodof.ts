4a636f8d299b7e224d77324c183cde8e7cbdfb68
Brodino <brodino96@gmail.com>
2026-02-19 10:09:45 +0100

    i'm going to restart everything again

--- src/utils/types/methodof.ts
+++
@@ -1,3 +0,0 @@
-export type MethodOf<T> = {
-	[K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
-}[keyof T]
--- src/utils/types/readonly.ts
+++
@@ -1,8 +0,0 @@
-// Only ChatGPT | Michigan TypeScript could defeat this shit 
-export type DeepReadonly<T> = {
-    readonly [K in keyof T]: T[K] extends (...args: any[]) => any
-      ? T[K] // If it's a function, keep it as is
-      : T[K] extends object
-      ? DeepReadonly<T[K]> // If it's an object or array, recurse
-      : T[K] // Otherwise, keep the property as is
-}
